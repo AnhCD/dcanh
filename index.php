8033246f79e5bc5ba86b10ab5e494f121611c8aa
Cong Anh <ohodavi1@gmail.com>
2019-06-11 17:00:05 +0700

    Update index.php
    them link vnexpess

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
   <title>Database Test</title>
  </head>
  <body>
- <?php echo '<p>Work on Database</p>'; 
- 
+ <?php echo '<p>Hello this is First cloud app</p>'; 
  ?> 
+<h1>Hello world</h1>
+<h2>This is cloud computing class</h2>
+<a href="vnexpess.net" target="_blank" >link to vnexpess</a>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
