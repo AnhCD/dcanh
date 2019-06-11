6dad6d30c9776a40306788884dd156e38a883ee7
Cong Anh <ohodavi1@gmail.com>
2019-06-11 17:01:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  ?> 
 <h1>Hello world</h1>
 <h2>This is cloud computing class</h2>
-<a href="vnexpess.net" target="_blank" >link to vnexpess</a>
+<a href="https://vnexpress.net/" target="_blank" >link to vnexpess</a>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
